32d311dfd5cc3a55d5bc363d8598f10b3bbad669
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-06-07 21:10:41 -0400

    Entidad usuario creado

---
+++ DAL/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace registroUsuario2.DAL
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+
+    }
+}
