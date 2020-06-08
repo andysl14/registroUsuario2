e0448f7baf31206930c30888d052783931dc06b9
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2020-06-07 21:16:05 -0400

    Arreglando ubicacion de entidad usuario

--- Entidades/Usuarios.cs
+++ Entidades/Usuarios.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace registroUsuario2.DAL
+namespace registroUsuario2.Entidades
 {
     public class Usuarios
     {
